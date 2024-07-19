a02412511bb4569d63829b5091247351861e7351
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-07-19 11:43:28 -0400

    chore: add sitemap and mdx astro's integrations and vite watch config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://example.com',
+    vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            }
+        }
+    },
+    integrations: [mdx(), sitemap()],
+});
