daf8f96863ad4109f8129d963db9be9d4207d5a1
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-07-19 17:50:03 -0400

    chore: change site title and description

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,13 +1,13 @@
 export const ui = {
   en: {
     "site.title": "Adonys Santos",
-    "site.description": "Welcome to my website!",
+    "site.description": "This is my personal website where I share my thoughts, projects, and experiences.",
     "nav.home": "Home",
     "nav.blog": "Blog",
   },
   es: {
-    "site.title": "Astro Blog",
-    "site.description": "Bienvenido a mi sitio web!",
+    "site.title": "Adonys Santos",
+    "site.description": "Este es mi sitio web personal donde comparto mis pensamientos, proyectos y experiencias.",
     "nav.home": "Inicio",
     "nav.blog": "Blog",
   },
