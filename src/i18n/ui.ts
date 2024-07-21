41d00ae9f13c5f15cb67fc1b03ebc3e563859b58
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-07-21 15:03:03 -0400

    chore: add nav translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -4,11 +4,17 @@ export const ui = {
     "site.description": "This is my personal website where I share my thoughts, projects, and experiences.",
     "nav.home": "Home",
     "nav.blog": "Blog",
+    "nav.social.github": "My GitHub profile",
+    "nav.social.linkedin": "My LinkedIn profile",
+    "nav.social.rss": "My blog blog RSS feed (in English)",
   },
   es: {
     "site.title": "Adonys Santos",
     "site.description": "Este es mi sitio web personal donde comparto mis pensamientos, proyectos y experiencias.",
     "nav.home": "Inicio",
     "nav.blog": "Blog",
+    "nav.social.github": "Mi perfil de GitHub",
+    "nav.social.linkedin": "Mi perfil de LinkedIn",
+    "nav.social.rss": "Mi blog RSS feed (en español)",
   },
 } as const;
