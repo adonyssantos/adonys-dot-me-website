363fcfeeb3a8869d713603b88a2046576f904bf2
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-10-15 22:30:51 -0400

    Revert "perf: eliminate render-blocking resources"
    This reverts commit 02a36235f7e1d1676d627d7ffd0c52de38703419.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,6 +21,6 @@ export default defineConfig({
         es: "es-ES",
       }
     }
-  })],
+  }), playformCompress()],
   prefetch: true
 });
