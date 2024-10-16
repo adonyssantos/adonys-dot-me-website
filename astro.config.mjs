02a36235f7e1d1676d627d7ffd0c52de38703419
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-10-15 22:24:26 -0400

    perf: eliminate render-blocking resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,6 +20,6 @@ export default defineConfig({
         es: "es-ES",
       }
     }
-  }), playformCompress()],
+  })],
   prefetch: true
 });
